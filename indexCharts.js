2ea4c6a315376262fb2f95b5b473cbd857fe3c04
unknown <jaypublicsharing@gmail.com>
2022-03-30 17:31:05 +0800

    first commit

--- indexCharts.js
+++ indexCharts.js
@@ -26,6 +26,9 @@ Highcharts.getJSON(dataPathHSI, function(data) {
         credits: {
             enabled: false
         },
+        scrollbar: {
+            enabled: false
+        },
 
         series: [{
                 name: 'HSi Index',
@@ -96,8 +99,176 @@ Highcharts.getJSON(dataPathJPY, function(data) {
             enabled: false
         },
 
+        rangeSelector: {
+            enabled: false
+        },
+
+        navigator: {
+            enabled: false
+        },
+        scrollbar: {
+            enabled: false
+        },
+
         series: [{
                 name: 'Yen',
+                data: data,
+                id: 'dataseries',
+                tooltip: {
+                    valueDecimals: 2
+                }
+            },
+            {
+                type: 'flags',
+                showInLegend: false,
+                data: [{
+                    x: Date.UTC(2010, 11, 1),
+                    title: 'A',
+                    text: 'Some event with a description'
+                }, {
+                    x: Date.UTC(2015, 11, 12),
+                    title: 'B',
+                    text: 'Some event with a description'
+                }, {
+                    x: Date.UTC(2017, 11, 22),
+                    title: 'C',
+                    text: 'Some event with a description'
+                }],
+                onSeries: 'dataseries',
+                shape: 'squarepin',
+                width: 16
+            }
+        ]
+    });
+});
+
+
+
+var dataPathShillerPE = '/macroview/data/data_MULTPL_SHILLER_PE_RATIO_MONTH.json'
+var containerShillerPE = 'containerShillerPE'
+Highcharts.getJSON(dataPathShillerPE, function(data) {
+    Highcharts.stockChart(containerShillerPE, {
+        rangeSelector: {
+            selected: 5
+        },
+
+        title: {
+            text: 'Shiller PE'
+        },
+
+        subtitle: {
+            text: 'More details here'
+        },
+
+        yAxis: {
+            title: {
+                text: 'Exchange rate'
+            }
+        },
+
+        legend: {
+            enabled: true
+        },
+        credits: {
+            enabled: false
+        },
+
+        rangeSelector: {
+            enabled: false
+        },
+
+        navigator: {
+            enabled: false
+        },
+        scrollbar: {
+            enabled: false
+        },
+
+        series: [{
+                name: 'Shiller PE',
+                data: data,
+                id: 'dataseries',
+                tooltip: {
+                    valueDecimals: 2
+                }
+            },
+            {
+                type: 'flags',
+                showInLegend: false,
+                data: [{
+                    x: Date.UTC(2010, 11, 1),
+                    title: 'A',
+                    text: 'Some event with a description'
+                }, {
+                    x: Date.UTC(2015, 11, 12),
+                    title: 'B',
+                    text: 'Some event with a description'
+                }, {
+                    x: Date.UTC(2017, 11, 22),
+                    title: 'C',
+                    text: 'Some event with a description'
+                }],
+                onSeries: 'dataseries',
+                shape: 'squarepin',
+                width: 16
+            }
+        ]
+    });
+});
+
+
+
+
+
+
+var dataPathCoreCPI = '/macroview/data/data_coreCPIYOY.json'
+var containerCoreCPI = 'containerCoreCPI'
+Highcharts.getJSON(dataPathCoreCPI, function(data) {
+    Highcharts.stockChart(containerCoreCPI, {
+        rangeSelector: {
+            selected: 5
+        },
+
+        title: {
+            text: 'Core CPI'
+        },
+
+        subtitle: {
+            text: 'More details here'
+        },
+
+        xAxis: {
+            scrollbar: {
+                enabled: false,
+            },
+        },
+
+        yAxis: {
+            title: {
+                text: 'Exchange rate'
+            }
+        },
+
+        legend: {
+            enabled: true
+        },
+        credits: {
+            enabled: false
+        },
+
+        rangeSelector: {
+            enabled: false
+        },
+
+        navigator: {
+            enabled: false
+        },
+        scrollbar: {
+            enabled: false
+        },
+
+        series: [{
+                name: 'Core CPI',
                 data: data,
                 id: 'dataseries',
                 tooltip: {
