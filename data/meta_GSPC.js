017f7ebbefc7d3975c360d9128a4eeb87bbc5a13
unknown <jaypublicsharing@gmail.com>
2022-03-31 23:37:40 +0800

    first commit

--- data/meta_GSPC.js
+++ data/meta_GSPC.js
@@ -1,1 +1,1 @@
-var meta = {"name": "GSPC", "currentUpdate": "31-03-22", "currentValue": 4597.27978515625, "minDate": "03-01-50", "minValue": 16.65999984741211, "maxDate": "31-03-22", "maxValue": 4796.56005859375, "lastUpdate": "31/03/2022 23:19:31", "dataFilename": "/macroview/data/data_GSPC.json"};
+var meta = {"name": "GSPC", "currentUpdate": "31-03-22", "currentValue": 4589.89013671875, "minDate": "03-01-50", "minValue": 16.65999984741211, "maxDate": "31-03-22", "maxValue": 4796.56005859375, "lastUpdate": "31/03/2022 23:36:31", "dataFilename": "/macroview/data/data_GSPC.json"};
