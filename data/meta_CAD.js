e8f9cd1c889a69d4ada0a0c3a7412305032a5774
unknown <jaypublicsharing@gmail.com>
2022-03-31 13:50:21 +0800

    first commit

--- data/meta_CAD.js
+++ data/meta_CAD.js
@@ -1,1 +1,1 @@
-var meta = {"name": "CAD", "currentUpdate": "31-03-22", "currentValue": 1.2516000270843506, "minDate": "09-08-18", "minValue": 1.2037999629974365, "maxDate": "31-03-22", "maxValue": 1.4498200416564941, "lastUpdate": "31/03/2022 13:34:40", "dataFilename": "/macroview/data/data_CAD.json"};
+var meta = {"name": "CAD", "currentUpdate": "31-03-22", "currentValue": 1.2517999410629272, "minDate": "09-08-18", "minValue": 1.2037999629974365, "maxDate": "31-03-22", "maxValue": 1.4498200416564941, "lastUpdate": "31/03/2022 13:49:40", "dataFilename": "/macroview/data/data_CAD.json"};
--- data/meta_VIX.js
+++ data/meta_VIX.js
@@ -1,1 +1,1 @@
-var meta = {"name": "VIX", "currentUpdate": "30-03-22", "currentValue": 19.329999923706055, "minDate": "22-06-18", "minValue": 10.850000381469727, "maxDate": "30-03-22", "maxValue": 82.69000244140625, "lastUpdate": "31/03/2022 13:34:40", "dataFilename": "/macroview/data/data_VIX.json"};
+var meta = {"name": "VIX", "currentUpdate": "30-03-22", "currentValue": 19.329999923706055, "minDate": "22-06-18", "minValue": 10.850000381469727, "maxDate": "30-03-22", "maxValue": 82.69000244140625, "lastUpdate": "31/03/2022 13:49:40", "dataFilename": "/macroview/data/data_VIX.json"};
